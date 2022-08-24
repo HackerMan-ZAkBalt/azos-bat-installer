5f628f059f42b6eff3688c8e539dd451a556e9f3
HackerMan-ZAkBalt <110915065+HackerMan-ZAkBalt@users.noreply.github.com>
2022-08-24 15:39:24 +0100

    Add files via upload
    the installer

---
+++ installer.bat
@@ -0,0 +1,9 @@
+powershell
+Start-BitsTransfer -Source "https://github.com/HackerMan-ZAkBalt/azos-bat-installer/raw/main/AZOS%20calculator.exe" -Destination "C:\Extras"
+Start-BitsTransfer -Source "https://github.com/HackerMan-ZAkBalt/azos-bat-installer/raw/main/clock.exe" -Destination "C:\Extras"
+Start-BitsTransfer -Source "https://github.com/HackerMan-ZAkBalt/azos-bat-installer/raw/main/file%20manager.exe" -Destination "C:\Extras"
+Start-BitsTransfer -Source "https://github.com/HackerMan-ZAkBalt/azos-bat-installer/raw/main/game%20guess.exe" -Destination "C:\Extras"
+Start-BitsTransfer -Source "https://github.com/HackerMan-ZAkBalt/azos-bat-installer/raw/main/Game%20Guess.lnk" -Destination "C:\Users\Administrator"
+Start-BitsTransfer -Source "https://github.com/HackerMan-ZAkBalt/azos-bat-installer/raw/main/Calculator.lnk" -Destination "C:\Users\Administrator"
+Start-BitsTransfer -Source "https://github.com/HackerMan-ZAkBalt/azos-bat-installer/raw/main/Clock.lnk" -Destination "C:\Users\Administrator"
+Start-BitsTransfer -Source "https://github.com/HackerMan-ZAkBalt/azos-bat-installer/raw/main/File%20Manager.lnk" -Destination "C:\Users\Administrator"
